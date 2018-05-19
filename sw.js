b87d0f5f10e862c448e7e01d3a8abcbd22688a38
AlanDeveloper <26407935+AlanDeveloper@users.noreply.github.com>
2018-05-19 15:16:49 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,39 +1,67 @@
-self.addEventListener('install', function(event) {
-  var indexPage = new Request('index.html');
-  var stylePage = new Request('css/style.css');
-  event.waitUntil(
-    fetch(indexPage).then(function(response) {
-      return caches.open('pwabuilder-offline').then(function(cache) {
-        return cache.put(indexPage, response);
-      });
-  }));
-  event.waitUntil(
-    fetch(stylePage).then(function(response) {
-      return caches.open('pwabuilder-offline').then(function(cache) {
-        return cache.put(stylePage, response);
-      });
-  }));
+//This is the service worker with the Cache-first network
+
+var CACHE = 'pwabuilder-precache';
+var precacheFiles = [
+      '/alandeveloper/',
+      '/alandeveloper/index.html',
+      '/alandeveloper/css/style.css'
+      /* Add an array of files to precache for your app */
+    ];
+
+//Install stage sets up the cache-array to configure pre-cache content
+self.addEventListener('install', function(evt) {
+  console.log('The service worker is being installed.');
+  evt.waitUntil(precache().then(function() {
+    console.log('[ServiceWorker] Skip waiting on install');
+      return self.skipWaiting();
+
+  })
+  );
 });
 
-self.addEventListener('fetch', function(event) {
-  var updateCache = function(request){
-    return caches.open('pwabuilder-offline').then(function (cache) {
-      return fetch(request).then(function (response) {
-        return cache.put(request, response);
-      });
+
+//allow sw to control of current page
+self.addEventListener('activate', function(event) {
+console.log('[ServiceWorker] Claiming clients for current page');
+      return self.clients.claim();
+
+});
+
+self.addEventListener('fetch', function(evt) {
+  console.log('The service worker is serving the asset.'+ evt.request.url);
+  evt.respondWith(fromCache(evt.request).catch(fromServer(evt.request)));
+  evt.waitUntil(update(evt.request));
+});
+
+
+function precache() {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.addAll(precacheFiles);
+  });
+}
+
+
+function fromCache(request) {
+  //we pull files from the cache first thing so we can show them fast
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      return matching || Promise.reject('no-match');
     });
-};
+  });
+}
 
-event.waitUntil(updateCache(event.request));
 
-event.respondWith(
-    fetch(event.request).catch(function(error) {
-      return caches.open('pwabuilder-offline').then(function (cache) {
-        return cache.match(event.request).then(function (matching) {
-          var report =  !matching || matching.status == 404?Promise.reject('no-match'): matching;
-          return report
-        });
-      });
-    })
-  );
-})
+function update(request) {
+  //this is where we call the server to get the newest version of the 
+  //file to use the next time we show view
+  return caches.open(CACHE).then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response);
+    });
+  });
+}
+
+function fromServer(request){
+  //this is the fallback if it is not in the cahche to go to the server and get it
+return fetch(request).then(function(response){ return response})
+}
