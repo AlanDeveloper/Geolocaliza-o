71d7eb0d859fb1d28514d60a382a63e96f5870fb
AlanDeveloper <26407935+AlanDeveloper@users.noreply.github.com>
2018-05-21 11:26:44 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,5 @@
 self.addEventListener('install', function(event) {
   var indexPage = new Request('index.html');
-  var offlinePage = new Request('offline.html');
-  event.waitUntil(
-    fetch(offlinePage).then(function(response) {
-      return caches.open('pwabuilder-offline').then(function(cache) {
-        console.log('[PWA Builder] Cached offline page during Install'+ response.url);
-        return cache.put(offlinePage, response);
-      });
-  }));
   event.waitUntil(
     fetch(indexPage).then(function(response) {
       return caches.open('pwabuilder-offline').then(function(cache) {
@@ -24,15 +16,6 @@ self.addEventListener('fetch', function(event) {
       });
     });
 };
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    fetch(event.request).catch(function(error) {
-        console.error( '[PWA Builder] Network request Failed. Serving offline page ' + error );
-        return caches.open('pwabuilder-offline').then(function(cache) {
-          return cache.match('offline.html');
-      });
-    }));
-});
 
 event.waitUntil(updateCache(event.request));
 
@@ -47,9 +30,3 @@ event.respondWith(
     })
   );
 })
-self.addEventListener('refreshOffline', function(response) {
-  return caches.open('pwabuilder-offline').then(function(cache) {
-    console.log('[PWA Builder] Offline page updated from refreshOffline event: '+ response.url);
-    return cache.put(offlinePage, response);
-  });
-});
